7b1723c1525989b919fdf71d5d9a018dafd01c7d
Laur04 <pmd7211@gmail.com>
2020-04-04 00:04:31 -0400

    fix

--- physics_quiz/apps/quiz/forms.py
+++ physics_quiz/apps/quiz/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class QuizForm(forms.Form):
 
     q1_choices = ((1, 'Making money'), (2, 'Helping other people'), (3, 'Having job security'), (4, 'Working with people'), (5, 'Having lots of family time'), (6, 'Having an exciting job'), (7, 'Making use of my talents/abilities'))
-    q2_choices = ((1, 'Medicine/Health'), (2, 'Biology'), (3, 'Chemistry'), (4, 'Physics'), (5, 'Astronomy'), (6, 'Engineering'), (7, 'English/Writing'), (8, 'Finance/Business/Consultancy'), (9, 'Administration/Management'), (10, 'Arts/Media'), (11, 'Academia/Education'))
+    q2_choices = (('a', 'Medicine/Health'), ('b', 'Biology'), ('c', 'Chemistry'), ('d', 'Physics'), ('e', 'Astronomy'), ('f', 'Engineering'), ('g', 'English/Writing'), ('h', 'Finance/Business/Consultancy'), ('i', 'Administration/Management'), ('j', 'Arts/Media'), ('k', 'Academia/Education'))
 
     question_1 = forms.MultipleChoiceField(error_messages={'required': 'You must select exactly three items for Question 1.'}, widget = forms.CheckboxSelectMultiple, choices=q1_choices)
     question_2 = forms.MultipleChoiceField(error_messages={'required': 'You must select exactly two items for Question 2.'}, widget = forms.CheckboxSelectMultiple, choices=q2_choices)
@@ -17,7 +17,7 @@ class QuizForm(forms.Form):
             self._errors = "You must select exactly three items for Question 1."
             return False
         for n in range(0, len(q1_ans)):
-            q1_ans[n] = int(q1_ans[n])
+            q1_ans[n] = str(q1_ans[n])
         q2_ans = self.cleaned_data['question_2']
         if len(q2_ans) != 2:
             self._errors = "You must select exactly two items for Question 2."
--- physics_quiz/apps/quiz/models.py
+++ physics_quiz/apps/quiz/models.py
@@ -5,7 +5,7 @@ from multiselectfield import MultiSelectField
 class Profile(models.Model):
 
     q1_choices = ((1, 'Making money'), (2, 'Helping other people'), (3, 'Having job security'), (4, 'Working with people'), (5, 'Having lots of family time'), (6, 'Having an exciting job'), (7, 'Making use of my talents/abilities'))
-    q2_choices = ((1, 'Medicine/Health'), (2, 'Biology'), (3, 'Chemistry'), (4, 'Physics'), (5, 'Astronomy'), (6, 'Engineering'), (7, 'English/Writing'), (8, 'Finance/Business/Consultancy'), (9, 'Administration/Management'), (10, 'Arts/Media'), (11, 'Academia/Education'))
+    q2_choices = (('a', 'Medicine/Health'), ('b', 'Biology'), ('c', 'Chemistry'), ('d', 'Physics'), ('e', 'Astronomy'), ('f', 'Engineering'), ('g', 'English/Writing'), ('h', 'Finance/Business/Consultancy'), ('i', 'Administration/Management'), ('j', 'Arts/Media'), ('k', 'Academia/Education'))
 
     name = models.CharField('Name', unique=True, max_length=100)
     job = models.CharField('Job Title', max_length=100)
