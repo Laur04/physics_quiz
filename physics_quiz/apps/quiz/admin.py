dc87e609dfb8b5e09346ff06f2fe30d31a8c736f
Laur04 <pmd7211@gmail.com>
2020-04-11 13:06:52 -0400

    register Answer in admin

--- physics_quiz/apps/quiz/admin.py
+++ physics_quiz/apps/quiz/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Profile, Option
+from .models import Profile, Option, Answer
 
 admin.site.register(Profile)
 admin.site.register(Option)
+admin.site.register(Answer)
