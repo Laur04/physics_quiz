f2d3d2bef51177e9221c3849fed25948efb876d5
Laur04 <pmd7211@gmail.com>
2020-04-23 20:32:25 -0400

    testing3

--- physics_quiz/apps/quiz/models.py
+++ physics_quiz/apps/quiz/models.py
@@ -51,7 +51,7 @@ class Profile(models.Model):
     def pic_name(self):
         a = self.name.lower()
         b = a.split()
-        return b
+        return b[0][0] + '_' + b[len(b) - 1] + '.png'
 
 class Option(models.Model):
     answers = (('a', 'Medicine/Health'), ('b', 'Biology'), ('c', 'Chemistry'), ('d', 'Physics'), ('e', 'Astronomy'), ('f', 'Engineering'), ('g', 'English/Writing'), ('h', 'Finance/Business/Consultancy'), ('i', 'Administration/Management'), ('j', 'Arts/Media'), ('k', 'Academia/Education'), ('1', 'Making money'), ('2', 'Helping other people'), ('3', 'Having job security'), ('4', 'Working with people'), ('5', 'Having lots of family time'), ('6', 'Having an exciting job'), ('7', 'Making use of my talents/abilities'))
