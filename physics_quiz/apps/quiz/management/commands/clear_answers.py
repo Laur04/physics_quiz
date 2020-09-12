ddfbefc362b4394ce209c554b34362654c3a5e7d
lauren <pmd7211@gmail.com>
2020-09-12 09:17:10 -0400

    fix delete command

--- physics_quiz/apps/quiz/management/commands/clear_answers.py
+++ physics_quiz/apps/quiz/management/commands/clear_answers.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from quiz.models import Answer
+from physics_quiz.apps.quiz.models import Answer
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
